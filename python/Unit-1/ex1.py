4d0bb973d2c7cbff91c3c448f397f68b1d945664
Menil-University <menil.jotaniya152296@marwadiuniversity.ac.in>
2026-08-21 10:39:39 +0530

    Delete python/Unit-1 directory

--- python/Unit-1/ex1.py
+++
@@ -1,6 +0,0 @@
-# Write a program to demonstrate basic input and output operations in Python
-
-name = input("Enter your name : ")
-rollno = int(input("Enter your Roll number : "))
-
-print("may name is : " , name + ", My Roll Number is :" , rollno)

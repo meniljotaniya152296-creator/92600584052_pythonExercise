e6d76f8e41ae84f525edcaaffda2e77d3fc569b9
Menil-University <menil.jotaniya152296@marwadiuniversity.ac.in>
2026-08-21 10:40:34 +0530

    unit-1 done

---
+++ Unit-1/ex1.py
@@ -0,0 +1,6 @@
+# Write a program to demonstrate basic input and output operations in Python
+
+name = input("Enter your name : ")
+rollno = int(input("Enter your Roll number : "))
+
+print("may name is : " , name + ", My Roll Number is :" , rollno)
